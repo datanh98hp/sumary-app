1d9a2ad39d5ff309e5289fef19b3995a3485b4fd
Datanh98hp <dat198hp@gmail.com>
2022-03-09 18:31:11 +0700

    "update UI"

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -91,6 +91,7 @@ export default function Header() {
             <button className="closebtn" onClick={()=>{setshowNav(false)}}>&times;</button>
             <a href="#" className="">
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" /></svg>
+              <p>Feed</p>
             </a>
             <a href="#" className="">
               <svg
@@ -107,12 +108,15 @@ export default function Header() {
                   d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
                 />
               </svg>
+              <p>Feed</p>
             </a>
             <a href="#" className="">
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" /></svg>
+              <p>Feed</p>
             </a>
             <a href="#" className="">
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" /></svg>
+              <p>Feed</p>
             </a>
             <a href="#" className="">
               <svg
@@ -129,6 +133,7 @@ export default function Header() {
                   d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                 />
               </svg>
+              <p>Feed</p>
             </a>
          
           </div>
--- src/component/Main/index.js
+++ src/component/Main/index.js
@@ -53,19 +53,19 @@ export default function Main() {
                             <li className='item-category'>
                                 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" /></svg><span>Top Top</span>
                             </li>
-                            <li className='item-category'>
-                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" /></svg><span>Top Top</span>
-                            </li>
+            
                         </ul>
                     </div>
                 </div>
-                <div className='main-middle col-s-9'>
-                    <Card />
+                <div className='main-middle col-s-8'>
+                    <div className='content'>
+                        <Card />
+                    </div>
                 </div>
                 <div className='main-right col-s-2'>
-                    <p>jd</p>
+                   
                     <p>Right</p>
-                    <p>jd</p>
+                   
                 </div>
             </div>
 
